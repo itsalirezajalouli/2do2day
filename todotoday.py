c3a429aaab64d33dceab1ddb4a8581bdebc0b4aa
Alireza Jalouli <154112092+itsalirezajalouli@users.noreply.github.com>
2024-09-26 20:39:59 +0330

    Update todotoday.py

--- todotoday.py
+++ todotoday.py
@@ -12,38 +12,43 @@ from colorama import init
 menu_options = ['New File', 'Load File', 'Exit']
 
 def print_menu(stdscr, selected_idx, menu_options):
-    stdscr.clear() # Clears the screen
-    h, w = stdscr.getmaxyx() # Get the height and width of the screen
+    stdscr.clear()  # Clears the screen
+    h, w = stdscr.getmaxyx()  # Get the height and width of the screen
     
-    figlet = pyfiglet.Figlet(font = 'slant')
+    figlet = pyfiglet.Figlet(font='slant')
     
     title = figlet.renderText('2do2day')
-
-    stdscr.addstr(title)
+    
+    # Use a color pair for the title
+    stdscr.addstr(title, curses.color_pair(2))  
 
     stdscr.addstr('\n[j]up | [k]down | [d]elete | [b]ack')
 
     for idx, option in enumerate(menu_options):
-        x = w//2 - len(option)//2
-        y = h//2 - len(menu_options)//2 + idx
+        x = w // 2 - len(option) // 2
+        y = h // 2 - len(menu_options) // 2 + idx
         if idx == selected_idx:
-            stdscr.addstr(y, x, option, curses.color_pair(1))
+            stdscr.addstr(y, x, option, curses.color_pair(1))  # Highlighted selection
         else:
-            stdscr.addstr(y,x, option)
+            stdscr.addstr(y, x, option, curses.color_pair(3))  # Unselected option
     stdscr.refresh()
 
 def main(stdscr):
     init()
     date_str = None
-    
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    # Initialize color pairs
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)  # Selected item
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)  # Title color
+    curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)  # Unselected item
 
     current_selected = 0
     curses.curs_set(0)
     print_menu(stdscr, current_selected, menu_options)
 
     while True:
-        #   Gets user input
+        # Gets user input
         key = stdscr.getch()
 
         if (key == curses.KEY_UP or key == ord('k')) and current_selected > 0:
@@ -51,12 +56,11 @@ def main(stdscr):
         elif (key == curses.KEY_DOWN or key == ord('j')) and current_selected < len(menu_options) - 1:
             current_selected += 1
         elif (key == curses.KEY_ENTER or key == ord(' ')) or key in [10, 13]:
-            stdscr.addstr(0,0, f"You selected '{menu_options[current_selected]}' Press ANY keys to continue ")
+            stdscr.addstr(0, 0, f"You selected '{menu_options[current_selected]}' Press ANY key to continue ")
             stdscr.refresh()
             stdscr.getch()
 
-            #   Here we check user's choice and go to related functions to the command
-
+            # Check user's choice
             if menu_options[current_selected] == 'Exit':
                 sys.exit()
             elif menu_options[current_selected] == 'New File':
@@ -75,7 +79,6 @@ def get_input(win, prompt_string):
     return input_str
 
 def get_task_number(filename):
-    # Creates the CSV file if it doesn't exist
     if not os.path.exists(filename):
         with open(filename, mode='w', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=['Number', 'Task', 'Time', 'Status'])
@@ -89,28 +92,27 @@ def get_task_number(filename):
     return max(task_numbers) + 1
 
 def Create(stdscr, date_str, fromLoad):
-    stdscr.clear() # Clears the screen
-    if date_str == None:
+    stdscr.clear()
+    if date_str is None:
         date_str = get_input(stdscr, 'File Name: ')
     if fromLoad:
         filename = date_str
     else:
-        if not isdir('./csvs'):
-            os.mkdir('./csvs')
+        if not isdir('./csvs/'):
+            os.mkdir('./csvs/')
         filename = f'{date_str}.csv'
-    filename = os.path.join('./csvs', filename)
+    filename = os.path.join('./csvs/', filename)
     tasks = []
     status = 'Not Started'
     i = 1
 
-    # Asks for input
     task = get_input(stdscr, 'Task: ')
     time = get_input(stdscr, 'Time: ')
     task_number = get_task_number(filename)
     try:
         with open(filename, mode='a', newline='') as file:
             writer = csv.DictWriter(file, fieldnames=['Number', 'Task', 'Time', 'Status'])
-            if file.tell() == 0:  # If file is empty, write headers
+            if file.tell() == 0:
                 writer.writeheader()
             writer.writerow({'Number': task_number, 'Task': task, 'Time': time, 'Status': status})
     except IOError as e:
@@ -120,14 +122,13 @@ def Create(stdscr, date_str, fromLoad):
         stdscr.getch()
         return
 
-    # Reload and show tasks after creating the new task
     stdscr.clear()
     view(stdscr, filename)
 
 def Loader(stdscr, date_str, current_selected):
-    if not os.path.isdir('./csvs'):
-        os.mkdir('./csvs')
-    dir = os.listdir('./csvs')
+    if not os.path.isdir('./csvs/'):
+        os.mkdir('./csvs/')
+    dir = os.listdir('./csvs/')
     dirList = [file for file in dir if file.endswith('.csv')]
     if not dirList:
         dirList = ['empty']
@@ -135,7 +136,6 @@ def Loader(stdscr, date_str, current_selected):
     print_menu(stdscr, current_selected, dirList)
 
     while True:
-        #   Gets user input
         key = stdscr.getch()
         
         if key == ord('b'):
@@ -146,7 +146,7 @@ def Loader(stdscr, date_str, current_selected):
             elif (key == curses.KEY_DOWN or key == ord('j')) and current_selected < len(dirList) - 1:
                 current_selected += 1
             elif key == ord('d'):
-                file_to_delete = os.path.join('./csvs', dirList[current_selected])
+                file_to_delete = os.path.join('./csvs/', dirList[current_selected])
                 try:
                     os.remove(file_to_delete)
                     dirList.pop(current_selected)
@@ -161,22 +161,22 @@ def Loader(stdscr, date_str, current_selected):
                 stdscr.refresh()
                 stdscr.getch()
             elif (key == curses.KEY_ENTER or key == ord(' ')) or key in [10, 13]:
-                stdscr.addstr(0,0, f"You selected '{dirList[current_selected]}' Press ANY keys to continue ")
+                stdscr.addstr(0, 0, f"You selected '{dirList[current_selected]}' Press ANY key to continue ")
                 stdscr.refresh()
                 stdscr.getch()
                 date_str = str(dirList[current_selected])
-                view(stdscr, os.path.join('./csvs', date_str))
+                view(stdscr, os.path.join('./csvs/', date_str))
         print_menu(stdscr, current_selected, dirList)
 
 def view(stdscr, date_str):
-    stdscr.clear() # Clears the screen
+    stdscr.clear()
     filename = date_str
     try:
         with open(filename, mode='r') as file:
             reader = csv.DictReader(file)
             tasks = [row for row in reader]
         stdscr.addstr('\n\n' + tabulate(tasks, headers='keys', tablefmt='rounded_outline')
-            + '\n\n [i]nsert | [e]dit | [t]ime edit | [d]elete | [m]ark completed | [b]ack')  # Displaying using tabulate
+            + '\n\n [i]nsert | [e]dit | [t]ime edit | [d]elete | [m]ark completed | [b]ack')
     except IOError as e:
         stdscr.clear()
         stdscr.addstr(0, 0, f"Error: {str(e)}")
@@ -209,7 +209,8 @@ def delete(stdscr, date_str):
     task_to_remove = get_input(stdscr, 'Number of Task to Remove: ')
     new_tasks = []
 
-    filename = os.path.join('./csvs', date_str)
+    # filename = os.path.join('./csvs/', date_str)
+    filename = date_str
 
     try:
         with open(filename, mode='r') as file:
@@ -217,15 +218,6 @@ def delete(stdscr, date_str):
             for row in reader:
                 if row['Number'] != task_to_remove:
                     new_tasks.append(row)
-
-        for idx, task in enumerate(new_tasks, start=1):
-            task['Number'] = idx
-
-        with open(filename, mode='w', newline='') as file:
-            fieldnames = ['Number', 'Task', 'Time', 'Status']
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(new_tasks)
     except IOError as e:
         stdscr.clear()
         stdscr.addstr(0, 0, f"Error: {str(e)}")
@@ -233,33 +225,32 @@ def delete(stdscr, date_str):
         stdscr.getch()
         return
 
-    stdscr.clear()
+    for idx, task in enumerate(new_tasks):
+        task['Number'] = idx + 1  # Re-number tasks
+
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['Number', 'Task', 'Time', 'Status'])
+        writer.writeheader()
+        writer.writerows(new_tasks)
+
     view(stdscr, date_str)
 
 def edit(stdscr, date_str):
     stdscr.clear()
-    task_to_edit = get_input(stdscr, 'Number of Task to Edit: ')
-    edit_to_what = get_input(stdscr, 'Change it to: ')
+    task_number = get_input(stdscr, 'Number of Task to Edit: ')
+    new_task = get_input(stdscr, 'New Task: ')
     new_tasks = []
 
-    filename = os.path.join('./csvs', date_str)
+    # filename =os.path.join('./csvs/', date_str)
+    filename = date_str
 
     try:
         with open(filename, mode='r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                if row['Number'] == task_to_edit:
-                    row['Task'] = edit_to_what
+                if row['Number'] == task_number:
+                    row['Task'] = new_task
                 new_tasks.append(row)
-
-        for idx, task in enumerate(new_tasks, start=1):
-            task['Number'] = idx
-
-        with open(filename, mode='w', newline='') as file:
-            fieldnames = ['Number', 'Task', 'Time', 'Status']
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(new_tasks)
     except IOError as e:
         stdscr.clear()
         stdscr.addstr(0, 0, f"Error: {str(e)}")
@@ -267,33 +258,28 @@ def edit(stdscr, date_str):
         stdscr.getch()
         return
 
-    stdscr.clear()
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['Number', 'Task', 'Time', 'Status'])
+        writer.writeheader()
+        writer.writerows(new_tasks)
+
     view(stdscr, date_str)
 
 def editTime(stdscr, date_str):
     stdscr.clear()
-    task_to_edit = get_input(stdscr, 'Number of Task to Edit Time: ')
-    edit_to_what = get_input(stdscr, 'Change it to: ')
+    task_number = get_input(stdscr, 'Number of Task to Edit Time: ')
+    new_time = get_input(stdscr, 'New Time: ')
     new_tasks = []
 
-    filename = os.path.join('./csvs', date_str)
+    filename = date_str
 
     try:
         with open(filename, mode='r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                if row['Number'] == task_to_edit:
-                    row['Time'] = edit_to_what
+                if row['Number'] == task_number:
+                    row['Time'] = new_time
                 new_tasks.append(row)
-
-        for idx, task in enumerate(new_tasks, start=1):
-            task['Number'] = idx
-
-        with open(filename, mode='w', newline='') as file:
-            fieldnames = ['Number', 'Task', 'Time', 'Status']
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(new_tasks)
     except IOError as e:
         stdscr.clear()
         stdscr.addstr(0, 0, f"Error: {str(e)}")
@@ -301,31 +287,29 @@ def editTime(stdscr, date_str):
         stdscr.getch()
         return
 
-    stdscr.clear()
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['Number', 'Task', 'Time', 'Status'])
+        writer.writeheader()
+        writer.writerows(new_tasks)
+
     view(stdscr, date_str)
-    
+
 def mark(stdscr, date_str):
-    filename = os.path.join('./csvs', date_str)
     stdscr.clear()
-    task_to_mark = get_input(stdscr, 'Number of Task You Completed: ')
+    task_number = get_input(stdscr, 'Number of Task to Mark Completed: ')
     new_tasks = []
+
+    # filename = os.path.join('./csvs/', date_str)
+
+    filename = date_str
 
     try:
         with open(filename, mode='r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                if row['Number'] == task_to_mark:
+                if row['Number'] == task_number:
                     row['Status'] = 'Completed'
                 new_tasks.append(row)
-
-        for idx, task in enumerate(new_tasks, start=1):
-            task['Number'] = idx
-
-        with open(filename, mode='w', newline='') as file:
-            fieldnames = ['Number', 'Task', 'Time', 'Status']
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(new_tasks)
     except IOError as e:
         stdscr.clear()
         stdscr.addstr(0, 0, f"Error: {str(e)}")
@@ -333,8 +317,11 @@ def mark(stdscr, date_str):
         stdscr.getch()
         return
 
-    stdscr.clear()
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=['Number', 'Task', 'Time', 'Status'])
+        writer.writeheader()
+        writer.writerows(new_tasks)
+
     view(stdscr, date_str)
 
-if __name__ == "__main__":
-    curses.wrapper(main)
+curses.wrapper(main)
